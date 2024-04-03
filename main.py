7ed38a0ce31bf307743493248c219896f60b2842
Daksh <pdaksh982@gmail.com>
2024-04-04 12:28:09 +1300

    test

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-<<<<<<< HEAD
-=======
-print("test123")
->>>>>>> f8953f88fae3942f4ba8f93a2efca1523428fd7d
+import time
+import sys
+
+#function used to print out words letter by letter
+def character_timer(string):  
+    for char in string:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.06)
+#function used to print out the instructions
+def instructions():
+    print("Welcome to the Area/Perimeter Calculator")
+    print()
+    print("This program's intended purpose is to calculate a shape's area and perimeter.")
+    print("Enter the shapes dimensions (measurements) and the program will output the answers for your shape's area and perimeter.")
+    print()
+
+#used to print out the shapes that can be calculated
+list = [ "- Circle\n","- Square\n","- Rectangle\n","- Triangle\n","- Parrallelogram\n" ]
+
+#instrutions will be printed here
+instructions()
+#list of shapes will be printed here
+print("These are the shapes that can be calculated:")
+time.sleep(2)
+character_timer("".join(list))
+
